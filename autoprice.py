b23258e146215cdbc6ef60bc19e08599746412e5
Mille Loo <74830754+Pinkvinus@users.noreply.github.com>
2023-05-25 14:14:12 +0200

    Update autoprice.py

--- autoprice.py
+++ autoprice.py
@@ -1,6 +1,5 @@
 import requests
 import os
-#import selenium
 import PyPDF2
 
 #from selenium import webdriver
@@ -29,6 +28,35 @@ def get_price(text:str):
     
     return 123
 
+def get_books(text:str):
+    """
+    params: A string of text that has been extracted from a pdf receipt
+    """
+
+    spliton1 = 'rykker'
+    spliton2 = 'ved gennemgang af bibliotekets udlån kan vi se'
+
+    if has_price(text):
+        spliton1 = 'pris\n'
+        spliton2 = 'samlet'
+    
+    books = text.split(spliton1)[1].strip().split(spliton2)[0].strip()
+    books = books.split('\n')
+
+
+    print(f'These are the books:{books}!!! wow such pretty books')
+
+def has_price(text:str)-> bool:
+    if "rykker" in text:
+        print(f'The books do not have a price')
+        return False
+    elif "pris" in text:
+        print(f'The books already have a price already has a price')
+        return True
+    else:
+        print('unable to determine if the books have a price')
+        return False
+
 def write_price_in_pdf(pdfFileObj):
     # creating a pdf file object
     
@@ -38,21 +66,32 @@ def write_price_in_pdf(pdfFileObj):
     if(len(pdfReader.pages)>1):
         print('Warning: There are multiple pages in this pdf. Only the first page will be looked at')
     
+    page0 = pdfReader.pages[0].extract_text(0).lower()
 
+    print(page0)
 
-
-    
-    page0 = pdfReader.pages[0].extract_text(0)
-    
-    if "Pris" or "pris" in page0:
-        print(f'The book {0} already has a price')
-    else:
-        get_price(page0)
+    get_books(page0)
     
     texts = str.split(page0, "\n")
-
-    print(texts)
     
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # URL from which pdfs to be downloaded
+    #URL = 'https://learnit.itu.dk/mod/resource/view.php?id=152145'
+    #get_pdf(URL)
+
+    sti = "C:/Users/Public/Documents/Regninger/Alma Paldan O'Brien - 3k - inkl. pris.pdf"
+    sti2 = "C:/Users/Public/Documents/Regninger/Asta Keogan Schlottmann - 3d.pdf"
+
+    pdfFileObj = open(sti2, 'rb')
+    write_price_in_pdf(pdfFileObj)
 
 
 
