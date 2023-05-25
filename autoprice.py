b695c680cb44f160ec355697335d00c5a7a5b450
Mille Loo <74830754+Pinkvinus@users.noreply.github.com>
2023-05-25 15:07:12 +0200

    Update autoprice.py

--- autoprice.py
+++ autoprice.py
@@ -1,36 +1,11 @@
-import requests
 import os
 import PyPDF2
+import re
 
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.common.by import By
-
-
-def get_pdf(url:str):
-    response = requests.get(url, stream=True)
-
-    # isolate PDF filename from URL
-    pdf_file_name = os.path.basename(url)
-    if response.status_code == 200:
-        # Save in current working directory
-        filepath = os.path.join(os.getcwd(), pdf_file_name)
-        with open(filepath, 'wb') as pdf_object:
-            pdf_object.write(response.content)
-            print(f'{pdf_file_name} was successfully saved!')
-            return True
-    else:
-        print(f'Uh oh! Could not download {pdf_file_name},')
-        print(f'HTTP response status code: {response.status_code}')
-        return False
-
-def get_price(text:str):
-    
-    return 123
-
-def get_books(text:str):
+def get_booksprice(text:str):
     """
     params: A string of text that has been extracted from a pdf receipt
+    returns: A list of book title and price pairs. If the price is not in the file, the price is set to None
     """
 
     spliton1 = 'rykker'
@@ -40,13 +15,22 @@ def get_books(text:str):
         spliton1 = 'pris\n'
         spliton2 = 'samlet'
     
-    books = text.split(spliton1)[1].strip().split(spliton2)[0].strip()
-    books = books.split('\n')
-
-
-    print(f'These are the books:{books}!!! wow such pretty books')
+    books = text.split(spliton1)[1].strip().split(spliton2)[0].strip().split('\n')
+    
+    bookprice = []
+    for b in books:
+        b = re.split(r'\s{1,}\d{10,} \d{4}-\d{2}-\d{2}\s', b)
+        if float(b[1]) == 2:
+            b = [b[0],None]
+        bookprice.append(b)
+        
+    return bookprice
 
 def has_price(text:str)-> bool:
+    """
+    params:  A text extracted from a PDF receipt
+    returns: A bool on whether the receipt contains a price
+    """
     if "rykker" in text:
         print(f'The books do not have a price')
         return False
@@ -57,41 +41,30 @@ def has_price(text:str)-> bool:
         print('unable to determine if the books have a price')
         return False
 
-def write_price_in_pdf(pdfFileObj):
-    # creating a pdf file object
-    
-    # creating a pdf reader object
-    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+def get_PDFreceipt_text(source:str)-> str:
+    """
+    Params:  A PDF receipt source
+    Returns: The text in the pdf
+    """
+    _,extension = os.path.splitext(source)
+    if extension != '.pdf':
+        raise TypeError("The file has to be a pdf-file")
 
+    pdfFileObj = open(source, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
     if(len(pdfReader.pages)>1):
         print('Warning: There are multiple pages in this pdf. Only the first page will be looked at')
     
     page0 = pdfReader.pages[0].extract_text(0).lower()
 
-    print(page0)
-
-    get_books(page0)
-    
-    texts = str.split(page0, "\n")
-    
-
-
-
-
-
-
+    return page0
 
 
 if __name__ == '__main__':
-    # URL from which pdfs to be downloaded
-    #URL = 'https://learnit.itu.dk/mod/resource/view.php?id=152145'
-    #get_pdf(URL)
-
     sti = "C:/Users/Public/Documents/Regninger/Alma Paldan O'Brien - 3k - inkl. pris.pdf"
     sti2 = "C:/Users/Public/Documents/Regninger/Asta Keogan Schlottmann - 3d.pdf"
 
-    pdfFileObj = open(sti2, 'rb')
-    write_price_in_pdf(pdfFileObj)
+    bp = get_booksprice(get_PDFreceipt_text(sti2))
 
 
 
